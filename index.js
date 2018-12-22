d10a5569c6ed22b3aec6ae0b46f1c70b2187d965
Alex Zdatny <alexzdatny@gmail.com>
2018-12-22 22:33:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+class Neighborhood {
+  constructor(name) {
+    this.id = ++NeighborhoodId;
+    this.name = name;
+  }
+
+  deliveries() {
+
+  }
+
+  customers() {
+
+  }
+
+  meals() {
+    
+  }
+}
